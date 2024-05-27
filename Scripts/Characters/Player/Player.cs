03d1c0ae03e3460b3e499d6412add3ad9fd51cd7
NMS <nicomonner@gmail.com>
2024-05-27 19:04:43 -0300

    HAGOV-1: Arreglo whitespace de Player.cs

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -3,36 +3,36 @@ using System;
 
 public partial class Player : Character
 {
-	public override void _Ready()
-	{
-		base._Ready();
-		GameEvents.OnReward += HandleReward;
-	}
+    public override void _Ready()
+    {
+        base._Ready();
+        GameEvents.OnReward += HandleReward;
+    }
 
 
 
 
-	public override void _Input(InputEvent @event)
-		{
-			direction = Input.GetVector(
-				GameConstants.INPUT_MOVE_LEFT, GameConstants.INPUT_MOVE_RIGHT, GameConstants.INPUT_MOVE_FOWARD, GameConstants.INPUT_MOVE_BACKWARD
-			);
-		}
+    public override void _Input(InputEvent @event)
+    {
+        direction = Input.GetVector(
+            GameConstants.INPUT_MOVE_LEFT, GameConstants.INPUT_MOVE_RIGHT, GameConstants.INPUT_MOVE_FOWARD, GameConstants.INPUT_MOVE_BACKWARD
+        );
+    }
 
-	private void HandleReward(RewardResource resource)
-	{
-		StatResource targetStat = GetStatResource(resource.TargetStat);
+    private void HandleReward(RewardResource resource)
+    {
+        StatResource targetStat = GetStatResource(resource.TargetStat);
 
-		targetStat.StatValue += resource.Amount;
-	}
+        targetStat.StatValue += resource.Amount;
+    }
 
-	public void ClampToCube(Vector3 cubePosition, Vector3 cubeSize)
-	{
-		Vector3 newPosition = GlobalPosition;
-		newPosition.X = Mathf.Clamp(GlobalPosition.X, cubePosition.X - cubeSize.X / 2, cubePosition.X + cubeSize.X / 2);
-		newPosition.Y = Mathf.Clamp(GlobalPosition.Y, cubePosition.Y - cubeSize.Y / 2, cubePosition.Y + cubeSize.Y / 2);
-		newPosition.Z = Mathf.Clamp(GlobalPosition.Z, cubePosition.Z - cubeSize.Z / 2, cubePosition.Z + cubeSize.Z / 2);
-		GlobalPosition = newPosition;
-	}
+    public void ClampToCube(Vector3 cubePosition, Vector3 cubeSize)
+    {
+        Vector3 newPosition = GlobalPosition;
+        newPosition.X = Mathf.Clamp(GlobalPosition.X, cubePosition.X - cubeSize.X / 2, cubePosition.X + cubeSize.X / 2);
+        newPosition.Y = Mathf.Clamp(GlobalPosition.Y, cubePosition.Y - cubeSize.Y / 2, cubePosition.Y + cubeSize.Y / 2);
+        newPosition.Z = Mathf.Clamp(GlobalPosition.Z, cubePosition.Z - cubeSize.Z / 2, cubePosition.Z + cubeSize.Z / 2);
+        GlobalPosition = newPosition;
+    }
 
 }
